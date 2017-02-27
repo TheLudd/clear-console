385791698b5b3e0669c1e1fd834cb7a0201105fc
Andy Edwards <jedwards@fastmail.com>
2017-02-27 10:03:08 -0600

    feat(index.js): add option to clear to start
    now if you call `clear({toStart: true})` it will clear to start (on my mac terminal at least), so that you can't scroll back to the cleared output.  When I'm running tests, linting, etc. in watch mode, I find it helpful to be able to nuke the old output so I can navigate through the latest errors as quickly as possible.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-module.exports = function () {
+module.exports = function (options) {
   process.stdout.write('\u001b[2J')
   process.stdout.write('\u001b[1;1H')
+  if (options && options.toStart) process.stdout.write('\u001b[3J')
 }
